a8325f288f29b3086f2da695e46b26dd1aad8b53
jwong1MHS <jasonw5172001@gmail.com>
2020-06-26 19:23:58 -0400

    Finished Problem 35

---
+++ Problem 35.py
@@ -0,0 +1,26 @@
+from sympy import isprime, nextprime
+from collections import deque
+
+limit = 1000000
+prime = 2
+circularList = []
+
+def circular_prime(num):
+    numList, rotationsList = [], []
+    for digit in str(num):  #adds each digit of the prime to a list
+        numList.append(digit)
+    A = deque(numList)
+    for i in range(len(A)): #creates each rotation of a number
+        rotationsList.append(int(''.join(A)))
+        A.rotate()
+    for number in rotationsList:    #checks if each number is prime
+        if (not isprime(number)):
+            return False
+    return True
+
+while (prime < limit):
+    if (circular_prime(prime)):
+        circularList.append(prime)
+    prime = nextprime(prime)
+
+print(len(circularList))
